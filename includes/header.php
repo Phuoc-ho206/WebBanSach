4876917784d49c86c0bf965087aa3446aa8213f6
Ho Ly Minh Phuoc <phlminh666@gmail.com>
2026-05-30 16:19:14 +0700

    đổi đường link sang link trực tiếp

--- includes/header.php
+++ includes/header.php
@@ -9,22 +9,22 @@ $pageTitle = $pageTitle ?? 'WebBanSach';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?></title>
-    <link rel="stylesheet" href="<?= asset('css/variables.css') ?>">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/variables.css">
 
     <!-- Layout -->
-    <link rel="stylesheet" href="<?= asset('css/layout/navbar.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/layout/footer.css') ?>">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/layout/navbar.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/layout/footer.css">
 
     <!-- Primitives -->
-    <link rel="stylesheet" href="<?= asset('css/primitives/button.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/form.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/card.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/badge.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/alert-toast.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/modal.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/pagination.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/table.css') ?>">
-    <link rel="stylesheet" href="<?= asset('css/primitives/spinner.css') ?>">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/button.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/form.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/card.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/badge.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/alert-toast.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/modal.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/pagination.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/table.css">
+    <link rel="stylesheet" href="/WebBanSach/assets/css/primitives/spinner.css">
 </head>
 
 <body>
